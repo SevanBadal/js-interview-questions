a427ee6e77987fe4008abdfc2a8ec3e519a6b690
SevanBadal <sgolnaza@gmail.com>
2023-11-19 15:26:00 -0500

    error handling

--- triple-add/triple-add.js
+++ triple-add/triple-add.js
@@ -2,6 +2,9 @@
 // tripleAdd(10)(20)(30) 
 
 const tripleAdd = x => y => z => {
+    if(isNaN(x) || isNaN(y) || isNaN(z)) {
+        throw new Error('Parameter is not a number!');
+    }
     return x + y + z
 }
 
